446e4174ce9c8ca17f29523d49b277d5d53f3967
EbolitaDevelopment <maldonado.alcala.leonardo@gmail.com>
2025-03-10 15:44:28 -0600

    CambioxAprobados

--- ElementosBasicosEstadistica/02Diccionario.py
+++ ElementosBasicosEstadistica/02Diccionario.py
@@ -11,6 +11,16 @@ def estadisticas(notas):
     notas.std()],index=['Minimo','Maximo','Media','Desviacion Estandar'])
     return estadistica
 
-notas = {'Juan':9,'Juanita':7,'Pedro':6.6,'Fabian':8.5,'Maximiliano':7.5,
+def aprobados(notas):
+    notas = pd.Series(notas)
+    return notas[notas>=6].sort_values(ascending=False)
+
+def Reprobados(notas):
+    notas = pd.Series(notas)
+    return aprobados.sort_values(ascending=False)
+
+notas = {'Juan':5.9,'Juanita':5,'Pedro':6.6,'Fabian':8.5,'Maximiliano':7.5,
          'Sandra':9.8,'Rosario':9}
+
 print(estadisticas(notas))
+print(aprobados(notas))
