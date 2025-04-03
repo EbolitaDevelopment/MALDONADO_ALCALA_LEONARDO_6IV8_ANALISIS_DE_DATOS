8899f5ae5d184c2708779a28356da95c1f479da4
EbolitaDevelopment <maldonado.alcala.leonardo@gmail.com>
2025-04-02 19:07:04 -0600

    Tarea

--- ElementosBasicosEstadistica/calculardistancia.py
+++ ElementosBasicosEstadistica/calculardistancia.py
@@ -1,4 +1,4 @@
-"""Calcularemos las distanicias entre todos los pares de puntos y determinaremos cuales están mas alejados entre si y cuales mas cercanos
+"""Calcularemos las distancias entre todos los pares de puntos y determinaremos cuales están mas alejados entre si y cuales mas cercanos
 utilizando las distancias euclidianas, manhattan y Chebyshev"""
 
 import numpy as np
@@ -15,39 +15,39 @@ puntos = {
     'Punto G':(4,6),
     'Punto H':(2,1)
 }
-#Convertir los puntos a un dataframe
+# Convertir los puntos a un dataframe
 df_puntos = pd.DataFrame(puntos).T
 df_puntos.columns = ['X','Y']
 print('Coordenadas de los puntos:')
 print(df_puntos)
 
-def calcular_distancia(df_puntos):
-    distancias  = pd.DataFrame(index=df_puntos.index,columns=df_puntos.index)
-    #Callcular las distancias entre todos los pares de puntos
+def calcular_distancia_generica(df_puntos, metrica):
+    """Función genérica para calcular distancias según la métrica especificada"""
+    distancias = pd.DataFrame(index=df_puntos.index, columns=df_puntos.index)
     for i in df_puntos.index:
         for k in df_puntos.index:
-            if i!=k:#No calcular la distancia entre el mismo punto
-                distancias.loc[i,k]=distance.euclidean(df_puntos.loc[i],df_puntos.loc[k])
+            if i != k:
+                distancias.loc[i,k] = metrica(df_puntos.loc[i], df_puntos.loc[k])
             else:
-                distancias.loc[i,k]=0
+                distancias.loc[i,k] = 0
     return distancias
-distancias = calcular_distancia(df_puntos)
-valor_maximo = distancias.values.max()
-(punto1,punto2) = distancias.stack().idxmax()
-print()
-print('Tabla de distancias entre los puntos:')
-print(distancias)
-print()
-print('La distancia maxima entre dos puntos es: ',valor_maximo)
-print()
-print('Los puntos mas alejados son: ',punto1,' y ',punto2)
 
-#Otra manera de calcular la distancia maxima entre dos puntos
-max_valor = distancias.max().max()
-col_max = distancias.max().idxmax()
-id_max=distancias[col_max].idxmax()
-print()
-print(f'Valor Máxima: {max_valor}')
-print(f'Columna: {col_max}')
-print(f'Indice: {id_max}')
-#Hacer con manhattan y chebyshev
+def mostrar_resultados(distancias, nombre_metrica):
+    """Función para mostrar resultados de manera consistente"""
+    valor_maximo = distancias.values.max()
+    punto1, punto2 = distancias.stack().idxmax()
+    
+    print(f'\nResultados para distancia {nombre_metrica}:')
+    print('Tabla de distancias entre los puntos:')
+    print(distancias)
+    print(f'\nLa distancia máxima entre dos puntos es: {valor_maximo}')
+    print(f'Los puntos más alejados son: {punto1} y {punto2}')
+
+# Calcular y mostrar resultados para cada métrica
+distancias_euclidean = calcular_distancia_generica(df_puntos, distance.euclidean)
+distancias_manhattan = calcular_distancia_generica(df_puntos, distance.cityblock)
+distancias_chebyshev = calcular_distancia_generica(df_puntos, distance.chebyshev)
+
+mostrar_resultados(distancias_euclidean, "Euclidiana")
+mostrar_resultados(distancias_manhattan, "Manhattan")
+mostrar_resultados(distancias_chebyshev, "Chebyshev")
